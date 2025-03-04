ce93f7d678e9e6658c76fc506fc72cd13a77b34d
june lee <melodysdreamj@gmail.com>
2025-03-04 23:27:56 +0900

    add gateway openrouter

---
+++ backend/cloudflare_gateway/openrouter_gemini_20_flash/note.ts
@@ -0,0 +1,15 @@
+import { requestCloudflareGatewayOpenRouterGemini20Flash } from "./_";
+
+async function main() {
+    console.log("start");
+
+    const response = await requestCloudflareGatewayOpenRouterGemini20Flash("Hello, how are you?");
+    console.log(response);
+}
+
+main().catch(err => {
+    console.error(err);
+    process.exit(1);
+});
+
+export {};
