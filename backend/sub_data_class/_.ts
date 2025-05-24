0670ff5f77c698137691d23bb04c302eadbf509f
june lee <melodysdreamj@gmail.com>
2025-05-24 22:48:58 +0900

    Create _.ts

---
+++ backend/sub_data_class/_.ts
@@ -0,0 +1,90 @@
+import { LegoUtil } from "../../util";
+
+export class New {
+  // s000: string = "";
+  // i000: number = 0;
+  // b000: boolean = false;
+  // r000: number = 0.0;
+  // t000: Date = new Date(0);
+  // l000: string[] = [];
+  // m000: { [key: string]: any } = {};
+  // c000: OtherModel = new OtherModel();
+  // j000 : OtherModel[] = [];
+  // e000: SomeEnum = SomeEnum.notSelected;
+
+  toDataString(): string {
+    return btoa(
+      Array.from(
+        new TextEncoder().encode(
+          new URLSearchParams({
+            // s000: this.s000,
+            // i000: this.i000.toString(),
+            // b000: this.b000.toString(),
+            // r000: this.r000.toString(),
+            // t000: this.t000.getTime().toString(),
+            // l000: JSON.stringify(this.l000),
+            // m000: JSON.stringify(this.m000),
+            // c000: this.c000.toDataString(),
+            // j000: JSON.stringify(this.j000.map((model: OtherModel) => model.toDataString())),
+            // e000: this.e000,
+          }).toString()
+        )
+      )
+        .map((byte) => String.fromCharCode(byte))
+        .join("")
+    );
+  }
+
+  static fromDataString(dataString: string): New {
+    const queryParams = Object.fromEntries(
+      new URLSearchParams(atob(dataString))
+    );
+
+    const object = new New();
+
+    // object.s000 = queryParams["s000"] || "";
+    // object.i000 = parseInt(queryParams["i000"] || "0", 10);
+    // object.b000 = queryParams["b000"] === "true";
+    // object.r000 = parseFloat(queryParams["r000"] || "0");
+    // object.t000 = new Date(parseInt(queryParams["t000"] || "0", 10));
+    // object.l000 = JSON.parse(queryParams["l000"] || "[]");
+    // object.m000 = JSON.parse(queryParams["m000"] || "{}");
+    // object.c000 = OtherModel.fromDataString(queryParams["c000"] || new OtherModel().toDataString());
+    // object.j000 = (JSON.parse(queryParams["j000"] || "[]") || []).map((item: string) => OtherModel.fromDataString(item));
+    // object.e000 = SomeEnumHelper.fromString(queryParams["e000"] || SomeEnum.notSelected);
+
+    return object;
+  }
+
+  toMap(): object {
+    return {
+      // s000: this.s000,
+      // i000: this.i000,
+      // b000: this.b000 ? 1 : 0,
+      // r000: this.r000,
+      // t000: this.t000.getTime(),
+      // l000: JSON.stringify(this.l000),
+      // m000: JSON.stringify(this.m000),
+      // c000: this.c000.toDataString(),
+      // j000: JSON.stringify(this.j000.map((model: OtherModel) => model.toDataString())),
+      // e000: this.e000,
+    };
+  }
+
+  static fromMap(queryParams: any): New {
+    const object = new New();
+
+    // object.s000 = queryParams.s000 || '';
+    // object.i000 = Number(queryParams.i000 || 0);
+    // object.b000 = queryParams.b000 === 1;
+    // object.r000 = queryParams.r000 || 0.0;
+    // object.t000 = new Date(queryParams.t000 || 0);
+    // object.l000 = JSON.parse(queryParams.l000 || '[]');
+    // object.m000 = JSON.parse(queryParams.m000 || '{}');
+    // object.c000 = OtherModel.fromDataString(queryParams.c000 || new OtherModel().toDataString());
+    // object.j000 = (JSON.parse(queryParams.j000 || '[]') || []).map((item: string) => OtherModel.fromDataString(item));
+    // object.e000 = SomeEnumHelper.fromString(queryParams.e000 || SomeEnum.notSelected);
+
+    return object;
+  }
+}
